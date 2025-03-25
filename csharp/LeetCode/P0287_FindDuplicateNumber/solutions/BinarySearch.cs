bab5f219d7333dd16b4b9b4fbd1e137dac7d6392
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-03-25 22:09:42 +0000

    add 0287. find the duplicate number: binary search solution

---
+++ csharp/LeetCode/P0287_FindDuplicateNumber/solutions/BinarySearch.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+
+namespace P0287_FindDuplicateNumber.BinarySearch
+{
+    public class Solution
+    {
+        // Binary search implementation
+        public int FindDuplicate(int[] nums)
+        {
+            int left = 1, right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;  // Avoid integer overflow
+                // Count numbers <= mid. If count > mid, then the duplicate number is in [1, mid]
+                int count = nums.Count(num => num <= mid);
+                if (count > mid)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            return left;
+        }
+    }
+}
