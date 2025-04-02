c0f2e1e07c0c5bb0dd3ccc058658224b33662d26
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-02 21:45:14 +0100

    update 0287. find the duplicate number: brute force

--- csharp/LeetCode/P0287_FindDuplicateNumber/solutions/BruteForce.cs
+++ csharp/LeetCode/P0287_FindDuplicateNumber/solutions/BruteForce.cs
@@ -1,22 +1,21 @@
-namespace P0287_FindDuplicateNumber.BruteForce
-{
-    // LeetCode convention: the class is named "Solution"
-    public class Solution
-    {
-        // Brute force implementation
-        public int FindDuplicate(int[] nums)
-        {
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
-                {
-                    if (nums[i] == nums[j])
-                        return nums[i];
-                }
-            }
+namespace P0287_FindDuplicateNumber.BruteForce;
 
-            // This should never happen
-            throw new ArgumentException("Input array must contain at least one duplicate");
+// LeetCode convention: the class is named "Solution"
+public class Solution
+{
+    // Brute force implementation
+    public int FindDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                    return nums[i];
+            }
         }
+
+        // This should never happen
+        throw new ArgumentException("Input array must contain at least one duplicate");
     }
 }
