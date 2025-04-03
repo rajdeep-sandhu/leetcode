c523100bdf7b78fd44caa0f02df98471dddfed32
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-03 22:12:02 +0100

    amend bit_manipulation.py to use explicit loops

--- python/p0287_find_duplicate_number/solutions/bit_manipulation.py
+++ python/p0287_find_duplicate_number/solutions/bit_manipulation.py
@@ -9,11 +9,18 @@ class Solution:
 
         for i in range(max_bits):
             bit_mask = 1 << i  # set the bit in position i.
-
+            count_nums = 0
+            count_range = 0
+            
             # Count the number of list elements with the bit set.
-            count_nums = sum(1 for num in nums if num & bit_mask)
+            for num in nums:
+                if num & bit_mask:
+                    count_nums += 1
+            
             # Count the number of elements in range with the bit set
-            count_range = sum(1 for num in range(1, n + 1) if num & bit_mask)
+            for num in range(1, n + 1):
+                if num & bit_mask:
+                    count_range += 1
 
             # If more numbers in the list have the bit set than expected, it is a duplicate.
             # Add the bit to the duplicate.
