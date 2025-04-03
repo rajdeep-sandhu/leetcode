b9297b9c6b3781c6deffc41ab06c00dca2667789
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-03 21:43:41 +0100

    update 0287. find the duplicate number: bit manipulation

---
+++ csharp/LeetCode/P0287_FindDuplicateNumber/solutions/BitManipulation.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+using System.Numerics;
+
+namespace P0287_FindDuplicateNumber.BitManipulation;
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        int n = nums.Length - 1;
+        int maxBits = sizeof(int) * 8 - BitOperations.LeadingZeroCount((uint)n);
+        int duplicate = 0;
+
+        for (int i = 0; i < maxBits; i++)
+        {
+            int bitMask = 1 << i;
+            int countNums = 0;
+            int countRange = 0;
+
+            foreach (int num in nums)
+            {
+                if ((num & bitMask) != 0)
+                    countNums++;
+            }
+
+            for (int num = 1; num <= n; num++)
+            {
+                if ((num & bitMask) != 0)
+                    countRange++;
+            }
+
+            if (countNums > countRange)
+                duplicate |= bitMask;
+        }
+        return duplicate;
+    }
+}
