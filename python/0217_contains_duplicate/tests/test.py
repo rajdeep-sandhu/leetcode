ff0af1b51ee7ce1ff7652b5d9cb8d975fd7b6f6e
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-05 22:57:42 +0100

    add 0217. contains duplicate tests

---
+++ python/0217_contains_duplicate/tests/test.py
@@ -0,0 +1,42 @@
+import importlib
+import json
+import pytest
+
+from pathlib import Path
+
+TESTS_DIR = Path(__file__).parent
+SOLUTIONS_DIR = TESTS_DIR.parent / "solutions"
+TEST_CASES_FILE = TESTS_DIR / "test_cases.json"
+
+
+SOLUTION_MODULES = [
+    f"{TESTS_DIR.parent.name}.solutions.{file.stem}"
+    for file in SOLUTIONS_DIR.glob("*.py")
+    if not file.stem.startswith("__")
+]
+
+def load_test_cases():
+    """Load test cases from json"""
+    with open(TEST_CASES_FILE, "r") as file:
+        return json.load(file)
+
+TEST_CASES = load_test_cases()
+
+
+@pytest.mark.parametrize("module_name", SOLUTION_MODULES)
+@pytest.mark.parametrize("test_case", TEST_CASES)
+def test_find_duplicate(module_name, test_case):
+    module = importlib.import_module(module_name)
+    solution = module.Solution()
+
+    nums = test_case["input"]
+    expected = test_case["expected"]
+    result = solution.containsDuplicate(nums)
+
+    print(f"Testing solution: {module_name}")
+    print(f"Input: {nums}, Expected: {expected}, Actual: {result}")
+    assert result == expected
+
+
+if __name__ == '__main__':
+    pytest.main(["-v"])  # verbose
