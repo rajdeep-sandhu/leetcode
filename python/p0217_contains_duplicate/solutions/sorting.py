f08dec6db1f68345acb06a4c5310735c9e5a97ba
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-19 21:37:27 +0100

    amend test.py logic for clarity

--- python/p0217_contains_duplicate/solutions/sorting.py
+++ python/p0217_contains_duplicate/solutions/sorting.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums = sorted(nums)
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
                 return True
         return False
