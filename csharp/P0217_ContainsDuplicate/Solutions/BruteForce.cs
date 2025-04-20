9728080551c667defe48634ed1a5034fe0b6d93e
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-20 15:15:59 +0100

    add BruteForce.cs

---
+++ csharp/P0217_ContainsDuplicate/Solutions/BruteForce.cs
@@ -0,0 +1,15 @@
+namespace P0217_ContainsDuplicate.Solutions.BruteForce;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+                if (nums[i] == nums[j])
+                    return true;
+        }
+        return false;
+    }
+}
