8d9bde0a13d034bf5978ccb02f6b9f913fa3343e
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-20 15:16:09 +0100

    add Sorting.cs

---
+++ csharp/P0217_ContainsDuplicate/Solutions/Sorting.cs
@@ -0,0 +1,14 @@
+namespace P0217_ContainsDuplicate.Solutions.Sorting;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (int i = 1; i < nums.Length; i++)
+            if (nums[i] == nums[i - 1]) return true;
+
+        return false;
+    }
+}
