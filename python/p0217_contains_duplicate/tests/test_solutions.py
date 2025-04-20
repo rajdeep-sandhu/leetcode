b48919c8ef2ab7a8e33f00d2bfafd75b329d076d
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-20 15:44:37 +0100

    amend test_solutions.py to handle case names

--- python/p0217_contains_duplicate/tests/test_solutions.py
+++ python/p0217_contains_duplicate/tests/test_solutions.py
@@ -29,13 +29,16 @@ def test_contains_duplicate(module_name, test_case):
     module = importlib.import_module(module_name)
     solution = module.Solution()
 
+    case_name = test_case.get("case_name", "Unnamed Case")
     nums = test_case["input"]
     expected = test_case["expected"]
     result = solution.containsDuplicate(nums)
 
-    print(f"Testing solution: {module_name}")
+    print(f"[Case: {case_name}]")
     print(f"Input: {nums}, Expected: {expected}, Actual: {result}")
-    assert result == expected
+    assert (
+        result == expected
+    ), f"{module_name} [{case_name}] failed. Input: {nums}, Expected: {expected}, Actual: {result}"
 
 
 if __name__ == '__main__':
