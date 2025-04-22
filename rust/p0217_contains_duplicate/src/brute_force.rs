a827399808d8bbec7a8551ed5483c829afbbefd9
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-22 09:07:37 +0100

    add brute_force.rs

---
+++ rust/p0217_contains_duplicate/src/brute_force.rs
@@ -0,0 +1,12 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                if nums[i] == nums[j] {return true;}
+            }
+        }
+    false
+    }
+}
