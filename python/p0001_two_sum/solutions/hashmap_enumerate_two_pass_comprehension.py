1a4416abf42adb63a5859d6f45feedba76273495
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-25 09:48:42 +0100

    add hashmap_enumerate_two_pass_comprehension.py

---
+++ python/p0001_two_sum/solutions/hashmap_enumerate_two_pass_comprehension.py
@@ -0,0 +1,14 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {num: i for i, num in enumerate(nums)}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen and seen[complement] != i:
+                return [i, seen[complement]]
