e90bb1b00cab396dc4a0188fcf4cab41446b3011
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-26 00:13:51 +0100

    add grouped_index_map_defaultdict.py

---
+++ python/p0001_two_sum/solutions/grouped_index_map_defaultdict.py
@@ -0,0 +1,20 @@
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = defaultdict(list)
+
+        for i, num in enumerate(nums):
+            seen[num].append(i)
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                for j in seen[complement]:
+                    if j != i:
+                        return [i, j]
