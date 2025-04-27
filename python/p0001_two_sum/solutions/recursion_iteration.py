e24570032f17284c89ab8e5dada8849a2516c324
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-27 09:20:05 +0100

    add recursion_iteration.py

---
+++ python/p0001_two_sum/solutions/recursion_iteration.py
@@ -0,0 +1,23 @@
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        def find_indices(i: int) -> List[int]:
+            
+            # Base case
+            if i >= len(nums):
+                return None
+            
+            # Recursive case
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+            
+            return find_indices(i + 1)
+        
+        return find_indices(0)
