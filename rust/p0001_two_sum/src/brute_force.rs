cfc9c82e5e3e5d0500ea762487ed68c91ca889ae
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-28 08:44:29 +0100

    add brute_force.rs

---
+++ rust/p0001_two_sum/src/brute_force.rs
@@ -0,0 +1,16 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() - 1 {
+            let complement: i32 = target - nums[i];
+
+            for j in i + 1..nums.len() {
+                if nums[j] == complement {
+                    return vec![i as i32, j as i32]
+                }
+            }
+        }
+        vec![]
+    }
+}
