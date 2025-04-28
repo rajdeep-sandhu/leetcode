8a6225432afce6858db9ff6f8d3d0d9079ab3b60
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-04-28 08:47:08 +0100

    remove dead code from helper.rs

--- rust/p0001_two_sum/src/helper.rs
+++ rust/p0001_two_sum/src/helper.rs
@@ -6,9 +6,3 @@ pub struct InputModel {
     pub target: i32,
 }
 
-#[allow(dead_code)]
-pub fn decode_input(raw_input: &str) -> InputModel {
-    let parsed: InputModel = serde_json::from_str(raw_input).unwrap();
-
-    parsed
-}
