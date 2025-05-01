2a24fc756526f1791b407e6d6906787cf0f8e842
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-01 11:05:36 +0100

    add grouped index map solution

---
+++ rust/p0001_two_sum/src/grouped_index_map.rs
@@ -0,0 +1,33 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+        // Build grouped index map.
+        let mut seen: HashMap<i32, Vec<usize>> = HashMap::new();
+
+        for (i, num) in nums.iter().enumerate() {
+            seen.entry(*num).or_insert_with(Vec::new).push(i);
+        }
+        
+        // Locate complement in grouped index map
+        for (i, num) in nums.iter().enumerate() {
+            let complement = target - num;
+
+            if let Some(indices) = seen.get(&complement) {
+                for &j in indices {
+                    if j != i {
+                        return vec![i as i32, j as i32]
+                    }
+                }
+            }
+        }
+
+        unreachable!("This should be unreachable");
+    }
+}
--- rust/p0001_two_sum/src/lib.rs
+++ rust/p0001_two_sum/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod brute_force;
 pub mod brute_force_enumerate;
 pub mod combinations;
+pub mod grouped_index_map;
 pub mod recursion_full;
 pub mod recursion_iteration;
 
