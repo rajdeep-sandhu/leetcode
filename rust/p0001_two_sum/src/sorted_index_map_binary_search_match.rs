30e66bb04f18e429be91b5ebeac4d5e37368087e
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-03 11:33:16 +0100

    correct code formatting

--- rust/p0001_two_sum/src/sorted_index_map_binary_search_match.rs
+++ rust/p0001_two_sum/src/sorted_index_map_binary_search_match.rs
@@ -5,7 +5,6 @@ pub struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-
         // Sort the numbers as (number, index) tuples to preserve original indices
         let mut sorted_map: Vec<(i32, usize)> =
             nums.iter().enumerate().map(|(i, &num)| (num, i)).collect();
@@ -13,31 +12,34 @@ impl Solution {
         sorted_map.sort_by_key(|&(num, _)| num);
 
         // Return index of search_val in sorted_map
-        fn binary_search(sorted_map: &[(i32, usize)],
-            mut left: usize, mut right: usize, search_val: i32) -> Option<usize> {
-                while left <= right {
-                    let mid = left + (right - left) / 2;
+        fn binary_search(
+            sorted_map: &[(i32, usize)], mut left: usize, mut right: usize, search_val: i32,
+        ) -> Option<usize> {
+            while left <= right {
+                let mid = left + (right - left) / 2;
 
-                    match sorted_map[mid].0.cmp(&search_val) {
-                        std::cmp::Ordering::Equal => return Some(mid),
-                        std::cmp::Ordering::Less => left = mid + 1,
-                        std::cmp::Ordering::Greater => {
-                            if mid == 0 { break; }  // Prevent underflow
-                            right = mid - 1;
-                        }
+                match sorted_map[mid].0.cmp(&search_val) {
+                    std::cmp::Ordering::Equal => return Some(mid),
+                    std::cmp::Ordering::Less => left = mid + 1,
+                    std::cmp::Ordering::Greater => {
+                        if mid == 0 {
+                            break;
+                        } // Prevent underflow
+                        right = mid - 1;
                     }
                 }
-                None
             }
+
+            None
+        }
 
         // Locate the complement
         for (i, &(num, index)) in sorted_map.iter().enumerate() {
             let complement = target - num;
 
             // Search only in the remaining vector slice
-            if let Some(j) = binary_search(&sorted_map,
-                i + 1, sorted_map.len() - 1, complement) {
-                    return  vec![index as i32, sorted_map[j].1 as i32];
+            if let Some(j) = binary_search(&sorted_map, i + 1, sorted_map.len() - 1, complement) {
+                return vec![index as i32, sorted_map[j].1 as i32];
             }
         }
 
