07fc4bac680f1a6a0bd0bb5df790e53ac74a40db
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-04 20:57:12 +0100

    add brute force solution using sorting

---
+++ python/p0242_valid_anagram/solutions/brute_force_sorting.py
@@ -0,0 +1,10 @@
+# Time complexity: O(n log n)
+# Space complexity: O(n)
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        return sorted(s) == sorted(t)
