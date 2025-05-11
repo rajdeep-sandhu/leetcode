643ac8cbad5b8f544fa657aaf678d30b4e0311ab
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-11 09:41:42 +0100

    add brute force solution

--- rust/p0242_valid_anagram/src/brute_force.rs
+++ rust/p0242_valid_anagram/src/brute_force.rs
@@ -1,10 +1,20 @@
-// Time complexity: 
-// Space complexity: 
+// Time complexity: O(n^2)
+// Space complexity: O(n)
 
 pub struct Solution;
 
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-        s == t
+        if s.len() != t.len() {
+            return false;
+        }
+
+        for letter in s.chars() {
+            if s.matches(letter).count() != t.matches(letter).count() {
+                return false;
+            }
+        }
+
+        true
     }
 }
