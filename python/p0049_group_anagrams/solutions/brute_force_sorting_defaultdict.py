3ea99e5b915e7235e35697acfbd15bf736bf48cc
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-14 00:12:14 +0100

    add brute force sorting solution using defaultdict

---
+++ python/p0049_group_anagrams/solutions/brute_force_sorting_defaultdict.py
@@ -0,0 +1,18 @@
+# Time complexity: O(n * k log k)
+# Space complexity: O(n)
+# where n = number of words, k = average length of a word
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagram_map: defaultdict = defaultdict(list)
+
+        for word in strs:
+            word_sorted: List = "".join(sorted(word))
+            anagram_map[word_sorted].append(word)
+
+        return list(anagram_map.values())
