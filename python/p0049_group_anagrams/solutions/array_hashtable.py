6f7ccd8b4e6d83a78b0b1499fa79811731c99691
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-14 00:12:32 +0100

    add array based hashtable solution

---
+++ python/p0049_group_anagrams/solutions/array_hashtable.py
@@ -0,0 +1,22 @@
+# Time complexity: O(n * k)
+# Space complexity: O(n * k)
+# where n = number of words, k = average length of a word
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagram_map: defaultdict = defaultdict(list)
+
+        for word in strs:
+            freq_map = [0] * 26
+
+            for letter in word:
+                freq_map[ord(letter) - ord("a")] += 1
+
+            anagram_map[tuple(freq_map)].append(word)
+
+        return list(anagram_map.values())
