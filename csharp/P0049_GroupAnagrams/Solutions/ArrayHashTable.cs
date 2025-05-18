d65da5781c4a2b1d107f5dbc27bad9f18c9b3d8e
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-05-18 10:48:03 +0100

    add array based hashtable and hashmap solution

---
+++ csharp/P0049_GroupAnagrams/Solutions/ArrayHashTable.cs
@@ -0,0 +1,33 @@
+// Time complexity: O(n * k)
+// Space complexity: O(n * k)
+// where n = number of words, k = length of the longest word// Space complexity: 
+
+namespace P0049_GroupAnagrams.Solutions.ArrayHashTable;
+
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        // Build anagram map
+        Dictionary<String, List<String>> anagramMap = new();
+
+        foreach (var word in strs)
+        {
+            // Build frequency map for each word
+            int[] frequencyMap = new int[26];
+
+            foreach (char letter in word)
+                frequencyMap[letter - 'a']++;
+
+            // Key needs to be a string
+            String key = String.Join(",", frequencyMap);
+
+            if (!anagramMap.ContainsKey(key))
+                anagramMap[key] = new List<String>();
+
+            anagramMap[key].Add(word);
+        }
+
+        return anagramMap.Values.Cast<IList<String>>().ToList();
+    }
+}
